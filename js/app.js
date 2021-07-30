fe8ed1a59fd6608d45bb12b19b46408595c635eb
Samouczek <795kasia@gmail.com>
2021-07-30 12:40:35 +0200

    add products and orders quantity

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 const calcForm = document.querySelector(".calc__form");
 const productsInput = calcForm.querySelector("#products");
+const ordersInput = calcForm.querySelector("#orders")
 
 const ulCalcSummary = document.querySelector(".calc__summary>ul")
 const productsOutput = ulCalcSummary.querySelector("li:first-child")
-console.log(productsOutput);
+const ordersOutput = ulCalcSummary.querySelector("li:nth-child(2)");
+console.log(ordersOutput);
 
 function Product(quantity){
     this.itemCalc = quantity + " * $0.5";
@@ -19,6 +21,23 @@ function Package(){
 }
 
 function validation(number){
-    return number > 0 || typeof (number) === Number;
+    return number >= 0 || typeof (number) === Number;
 }
 
+productsInput.addEventListener("keyup", function (event){
+    if (validation(this.value)){
+           productsOutput.style.display = "flex";
+           let product = new Product(this.value);
+           productsOutput.children[1].innerText = product.itemCalc;
+           productsOutput.children[2].innerText = "$"+product.itemPrice;
+    }
+});
+
+ordersInput.addEventListener("keyup", function (event){
+    if (validation(this.value)){
+        ordersOutput.style.display = "flex";
+        let order = new Product(this.value);
+        ordersOutput.children[1].innerText = order.itemCalc;
+        ordersOutput.children[2].innerText = "$"+order.itemPrice;
+    }
+});
